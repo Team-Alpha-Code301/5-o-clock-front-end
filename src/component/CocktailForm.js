b6976d8498fb3b0d5bed3f8993c54d159029ed44
Gordon P Reilley Jr <gordie378@gmail.com>
2022-12-02 15:02:05 -0500

    Working on form

---
+++ src/component/CocktailForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+class CocktailForm extends React.Component {
+  handleGetCocktails = (e) => {
+    e.preventDefault();
+    let selectedIngredient = e.target.ingredient.value;
+    console.log(selectedIngredient);
+    this.props.getCocktails(selectedIngredient);
+  };
+  render() {
+    return (
+      <Form onSubmit={this.handleGetCocktails}>
+        <Form.Group className="cocktailForm" controlId="ingredient">
+          <Form.Label>Type Your Ingredient</Form.Label>
+          <Form.Control type="text" placeholder="Enter Ingredient Name" />
+        </Form.Group>
+        <Button type='submit'>Submit</Button>
+      </Form>
+    );
+  }
+}
+
+export default CocktailForm;
