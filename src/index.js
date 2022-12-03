2d5b5e774bacf9d594eec292e092e138325aeedf
brenda70904 <brendaj0214@gmail.com>
2022-12-03 13:02:54 -0800

    fix the redirect URI

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_DOMAIN}
     clientId={process.env.REACT_APP_CLIENT_ID}
-    redirectUri={window.location.origin}
+    redirectUri={process.env.REACT_APP_REDIRECT_URI}
   >
     <App />
   </Auth0Provider>
